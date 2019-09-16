7dae3f2c67d30d6a604543d3c382016725d597f8
rinoy <rinoy@promactinfo.com>
2019-09-16 11:18:06 +0530

    Model created.

---
+++ SportsTestApp1609/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsTestApp1609.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
