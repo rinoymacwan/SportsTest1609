f14e44c0e4478151f37e5267755a359399bb0ec7
rinoy <rinoy@promactinfo.com>
2019-09-16 11:10:50 +0530

    Add project files.

---
+++ SportsTestApp1609/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportsTestApp1609.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
